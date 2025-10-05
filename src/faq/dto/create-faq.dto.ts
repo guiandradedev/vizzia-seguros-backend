fece6ff69ee1132759bb9fad94174fe02cf0e215
PG2zinho <pedro-gasparotto@outlook.com>
2025-10-05 19:02:34 -0300

    arrumei o arquivo dto

--- src/faq/dto/create-faq.dto.ts
+++ src/faq/dto/create-faq.dto.ts
@@ -4,13 +4,12 @@ export class CreateFaqDto {
 
     @IsNotEmpty()
     @IsString()
-    @MaxLength(150, {message: 'Mensagem não pode ter mais que 150 caracteres'})
+    @MaxLength(255, {message: 'Mensagem não pode ter mais que 150 caracteres'})
     @MinLength(20, {message: 'A menssagem não pode ter menos que 20 caracteres'})
     question:string;
 
     @IsNotEmpty()
     @IsString()
-    @MaxLength(150, {message: 'Mensagem não pode ter mais que 150 caracteres'})
     @MinLength(20, {message: 'A menssagem não pode ter menos que 20 caracteres'})
     answer: string;
 
