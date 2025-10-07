e9916f82dc4dab809cbf6225a6d389c281ef400b
LuigiZanon <lugazabugre@outlook.com>
2025-10-06 22:10:54 -0300

    authguard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,8 @@
-import { forwardRef, Global, Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { HashingServiceProtocol } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
-import { UsersService } from 'src/users/users.service';
 import { ConfigModule } from '@nestjs/config';
 import jwtConfig from './config/jwt.config';
 import { JwtModule } from '@nestjs/jwt';
@@ -14,8 +13,7 @@ import { UsersModule } from 'src/users/users.module';
   imports: [
     UsersModule,
     ConfigModule.forFeature(jwtConfig),
-    JwtModule.registerAsync(jwtConfig.asProvider())
-
+    JwtModule.registerAsync(jwtConfig.asProvider()),
   ],
   controllers: [AuthController],
   providers: [AuthService, {
@@ -23,6 +21,6 @@ import { UsersModule } from 'src/users/users.module';
     useClass: BcryptService, 
     }
   ],
-  exports: [HashingServiceProtocol, AuthService],
+  exports: [HashingServiceProtocol, AuthService, JwtModule, ConfigModule],
 })
 export class AuthModule {}
